c5b2beed179d40a69918f225cfe3243b33093c3d
Matthew Treinish <mtreinish@kortar.org>
2017-05-07 15:47:22 -0400

    Cleanup load graphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -5,7 +5,7 @@ import pandas
 ram_usage = pandas.read_csv('manual_load_test_memory_usage.csv')
 dates = ram_usage['Date']
 del ram_usage['Date']
-ram_usage =  ram_usage.set_index(pandas.to_datetime(dates))
+ram_usage = ram_usage.set_index(pandas.to_datetime(dates))
 cpu_usage = pandas.read_csv('manual_load_test_cpu_usage.csv')
 
 cpu_dates = cpu_usage['Date']
@@ -17,10 +17,18 @@ print(cpu_usage)
 
 # Plot area usage
 plt.figure()
-ram_usage.plot.area(stacked=True)
+ram_usage.plot.area(stacked=True, colormap='Dark2')
+axes = plt.gca()
+axes.set_ylim([0, 2096214016])
+#plt.title('Memory Usage During Manual Load Test')
+plt.ylabel('Used Memory')
 plt.savefig('manual_load_ram_usage.png', dpi=900)
 
 # Plot cpu usage
 plt.figure()
 cpu_usage.plot.area()
+axes = plt.gca()
+axes.set_ylim([0, 100])
+#plt.title('CPU Usage During Manual Load Test')
+plt.ylabel('CPU Load Percentage')
 plt.savefig('manual_load_cpu_usage.png', dpi=900)
